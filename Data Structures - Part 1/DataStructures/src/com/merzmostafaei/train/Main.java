50e7867e4fe1f32a6033e7c282b99cc7ed983719
merz mostafaei <merzmostafaei@gmail.com>
2025-08-25 09:47:30 -0200

    Implementation Heap and Remove.

--- Data Structures - Part 1/DataStructures/src/com/merzmostafaei/train/Main.java
+++ Data Structures - Part 1/DataStructures/src/com/merzmostafaei/train/Main.java
@@ -413,7 +413,7 @@ public class Main {
         heap.insert(17);
         heap.insert(4);
         heap.insert(22);
-
+        System.out.println(heap.remove());
         System.out.println(heap);
 
 
--- Data Structures - Part 1/DataStructures/src/com/merzmostafaei/train/heap/buildingAheap.java
+++ Data Structures - Part 1/DataStructures/src/com/merzmostafaei/train/heap/buildingAheap.java
@@ -12,7 +12,7 @@ public class buildingAheap {
         if (isFull())
             throw new IllegalStateException();
 
-        array[size]= node;
+        array[size] = node;
         size++;
         bubbleUp();
 
@@ -40,9 +40,78 @@ public class buildingAheap {
         array[second] = tmp;
     }
 
+
     private boolean isFull() {
         return size == array.length;
     }
+
+    public int remove(){
+        if (isEmpty())throw new IllegalStateException();
+
+        var root = array[0];
+        array[0] = array[--size];
+        bubbleDown();
+        return root;
+    }
+
+    private void bubbleDown() {
+        var index = 0;
+
+        while (index <= size && !isValidParent(index)){
+
+            var LargChildIndex = largerChildIndex(index);
+            swap(index,lefChildIndex(index));
+            index = LargChildIndex;
+        }
+    }
+
+    private boolean isValidParent(int index){
+        if (!hasLeftChild(index))
+            return true;
+
+        var isValid = array[index] > leftChild(index);
+
+        if (hasRightChild(index))
+            isValid &= array[index] > rightChild(index);
+
+        return isValid;
+    }
+    private int largerChildIndex(int index){
+        if (hasLeftChild(index))return index;
+        if(hasRightChild(index))return lefChildIndex(index);
+
+        return (leftChild(index) > rightChild(index) ? lefChildIndex(index) : rightChildIndex(index));
+
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    private int rightChild(int index){
+        return array[rightChildIndex(index)];
+    }
+
+    private int leftChild(int index) {
+        return array[lefChildIndex(index)];
+    }
+
+    private int lefChildIndex(int index){
+        return 2 * index + 1;
+    }
+    private int rightChildIndex(int index){
+        return 2 * index + 2;
+    }
+
+    private boolean hasLeftChild(int index){
+        return lefChildIndex(index) <= size;
+    }
+
+    private boolean hasRightChild(int index){
+        return rightChildIndex(index) <= size;
+    }
+
+
 
     @Override
     public String toString() {
